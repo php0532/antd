a68c0833453ca4f40f54380fe9d512c2965a46a3
zhangyong01 <zhangyong01@smzdm.com>
2019-05-04 16:44:51 +0800

    init

---
+++ config/config.js
@@ -0,0 +1,12 @@
+export default {
+    singular: true,
+    plugins:[
+        ['umi-plugin-react',{
+            //  暂无
+        }],
+    ],
+    routes:[{
+        path: '/',
+        component: './HelloWorld',
+    }],
+};
